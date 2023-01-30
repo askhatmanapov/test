12fcad29e03ff31b41d85dd8895e37f439d41832
Askhat Manapov <askhatmanapov.02@gmail.com>
2023-01-30 16:14:10 +0600

    modified:   index.html 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
             <input id="1-1" type="radio" name="q1" value="Эндокринолог" v-model="option1"><label for="1-1">Усталость</label><br>
             <input id="1-2" type="radio" name="q1" value="Эндокринолог" v-model="option1"><label for="1-2">Апатия</label>
         </ul>
-        <!-- <ul>{{ option1 }}</ul> -->
         <button type="button" v-on:click="options.push(option1)" @click="check">Дальше</button>
     </div>
 
@@ -141,7 +140,6 @@
             return{
                 options: [],
                 status: 1,
-                // option1: '',
                 missing: 0
             }
         },
